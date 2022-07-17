8b39883248e5d9d7c9e4e1784ba8787fd2d8f539
dimassdoubles <codedimas@gmail.com>
2022-07-18 01:34:11 +0700

    refactor: memindahkan widget DetailScreen ke file detail_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
+import 'screens/detail_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,79 +16,6 @@ class MyApp extends StatelessWidget {
       title: 'Wisata Bandung',
       theme: ThemeData(),
       home: DetailScreen(),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              child: Text(
-                "Farm House Lembang",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-                ),
-              margin: EdgeInsets.only(top: 16),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Icon(Icons.calendar_today),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text('Open Everyday'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.access_time),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text('09.00 - 20.00'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.monetization_on),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text('Rp 25.000'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Text(
-                'Berada di jalur Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
